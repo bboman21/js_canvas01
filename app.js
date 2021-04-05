668fff8e1725ab5e3876b41655d98578fdf50021
bboman21 <bboman21@gmail.com>
2021-04-06 08:57:55 +0900

    modified

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-const canvas = document.getElementById("jsCanvas");
+//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
 function onMouseMove(event) {
-    
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y);
-
+    //console.log(x, y);
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
+
+
+
+function onMouseDown(event) {
+    //console.log(event)
+    painting = true;
+}
+
+
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting); 
+    canvas.addEventListener("mouseleave", stopPainting);
 }
